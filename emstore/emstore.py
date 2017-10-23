427166084754c4ff3add4184f625eb8085aecadf
Michal Mucha <mjmucha@icloud.com>
2017-10-23 00:13:25 -0600

    stubs

--- emstore/emstore.py
+++ emstore/emstore.py
@@ -1,2 +1,38 @@
-class Emstore:
-    pass
+class Emstore(object):
+    """docstring for Emstore"""
+
+    def __init__(self, arg):
+        super(Emstore, self).__init__()
+        self.arg = arg
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __contains__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+
+def _read(key, db):
+    """Read from leveldb and return array of floats.
+
+    key.encode('utf8')
+    embedding = db.get(key)
+    embedding.split(b' ')
+    return [float(e) for e in embedding]
+
+    """
