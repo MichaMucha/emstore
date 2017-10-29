e0f82e963b5c0b43043650ac21e6dcfb304b2d47
Michal Mucha <mjmucha@icloud.com>
2017-10-29 16:44:40 -0400

    create glove embeddings db download glove tests

--- emstore/glove.py
+++ emstore/glove.py
@@ -1,6 +1,64 @@
-def download():
-    pass
+import os
+import warnings
+from subprocess import call
+
+import requests
+from tqdm import tqdm
+
+from .create import create_embedding_database
+
+DEFAULT_GLOVE_DOWNLOAD_PATH = os.path.join('/', 'tmp', 'glove', 'glove.zip')
 
 
-def create():
-    pass
+def download(target_path=None, url=None):
+    """Download GloVe Common Crawl 840B tokens.
+
+    (840B tokens, 2.2M vocab, cased, 300d vectors, 2.03 GB download)
+
+    args:
+        target_path: where to save
+        default /tmp/glove/glove.zip
+
+        url: where to downlad from
+        default http://nlp.stanford.edu/data/glove.840B.300d.zip
+
+    """
+    if target_path is None:
+        target_path = DEFAULT_GLOVE_DOWNLOAD_PATH
+        try:
+            os.makedirs(os.path.dirname(DEFAULT_GLOVE_DOWNLOAD_PATH))
+        except FileExistsError:
+            pass
+    if url is None:
+        url = 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
+    response = requests.get(url, stream=True)
+    with open(target_path, 'wb') as f:
+        pbar = tqdm(
+            unit="KB", total=int(response.headers['Content-Length']) // 1024)
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                pbar.update(len(chunk) // 1024)
+                f.write(chunk)
+
+
+def create(embeddings_file=None, path_to_db=None, keep_file=True):
+    if embeddings_file is None:
+        if os.path.exists(DEFAULT_GLOVE_DOWNLOAD_PATH):
+            embeddings_file = DEFAULT_GLOVE_DOWNLOAD_PATH
+        else:
+            warnings.warn('''GloVe embeddings file path not specified,
+            archive not found at default path.
+            Commencing 2.03GB download.
+            File will be deleted after DB is created.
+            //default path is:
+            {}
+            '''.format(DEFAULT_GLOVE_DOWNLOAD_PATH))
+            download()
+            embeddings_file = DEFAULT_GLOVE_DOWNLOAD_PATH
+            keep_file = False
+    if path_to_db is None:
+        path_to_db = os.path.expanduser(os.path.join('~', 'glove'))
+
+    create_embedding_database(embeddings_file, path_to_db)
+    if keep_file is False:
+        call(['rm', '-f', embeddings_file])
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,15 +1,48 @@
 # -*- coding: utf-8 -*-
 
+import os
 import unittest
+from subprocess import call
 
 from .context import emstore
 
+TEST_GLOVE_DIR = os.path.join('/', 'tmp', 'glove')
 
-class AdvancedTestSuite(unittest.TestCase):
-    """Advanced test cases."""
 
-    def test_thoughts(self):
-        self.assertIsNone(None)
+class DownloadGloveTestSuite(unittest.TestCase):
+    """Downlad glove test cases."""
+
+    def setUp(self):
+        super().setUp()
+        try:
+            os.makedirs(TEST_GLOVE_DIR)
+        except FileExistsError:
+            pass
+
+    def test_download_fn(self):
+        test_url = 'https://nlp.stanford.edu/pubs/glove.pdf'
+        test_target_path = os.path.join(TEST_GLOVE_DIR, 'test.pdf')
+        emstore.glove.download(target_path=test_target_path, url=test_url)
+        self.assertTrue(os.path.exists(test_target_path))
+
+    def test_download_glove1000(self):
+        test_url = 'github.com/glove_1000.zip'
+        test_target_path = os.path.join(TEST_GLOVE_DIR, 'glove.zip')
+        emstore.glove.download(target_path=test_target_path, url=test_url)
+        self.assertTrue(os.path.exists(test_target_path))
+
+    def test_create_glove(self):
+        test_target_path = os.path.join(TEST_GLOVE_DIR, 'glove.zip')
+        test_db_path = os.path.join(TEST_GLOVE_DIR, 'test_db')
+        emstore.glove.create(
+            embeddings_file=test_target_path, path_to_database=test_db_path)
+        self.assertTrue(os.path.exists(test_db_path))
+        embeddings = emstore.Emstore(test_db_path)
+        self.assertFalse(embeddings.closed)
+        embeddings.close()
+
+    def tearDown(self):
+        call(['rm', '-rf', TEST_GLOVE_DIR])
 
 
 if __name__ == '__main__':
